9e3ed4b75ab031bd76b4e859fe1fd85812bfe43b
smarcosm <sebastiaomarcos49@gmail.com>
2023-10-09 21:24:07 -0300

    Desafio UML

---
+++ src/Iphone.java
@@ -0,0 +1,56 @@
+public class Iphone implements NavegadorInternet, AparelhoTelefonico, ReprodutorMusical{
+    public static void main(String[] args) {
+
+    Iphone iphone = new Iphone();
+    iphone.ligar();
+    iphone.selecionarMusica();
+    iphone.tocar();
+    iphone.exibirPagina();
+
+    }
+
+    @Override
+    public void ligar() {
+        System.out.println("Ligando...");
+    }
+
+    @Override
+    public void atender() {
+        System.out.println("Atendendo...");
+    }
+
+    @Override
+    public void iniciarCorreioVoz() {
+        System.out.println("Iniciando correio de voz...");
+    }
+
+    @Override
+    public void exibirPagina() {
+        System.out.println("Exibindo página...");
+    }
+
+    @Override
+    public void adicionarPagina() {
+        System.out.println("Adicionando página...");
+    }
+
+    @Override
+    public void atualizarPagina() {
+        System.out.println("Atualizando página...");
+    }
+
+    @Override
+    public void tocar() {
+        System.out.println("Tocando música...");
+    }
+
+    @Override
+    public void pausar() {
+        System.out.println("Pausando música...");
+    }
+
+    @Override
+    public void selecionarMusica() {
+        System.out.println("Selecionando música...");
+    }
+}
---
+++ src/NavegadorInternet.java
@@ -0,0 +1,5 @@
+public interface NavegadorInternet {
+    public void exibirPagina();
+    public void adicionarPagina();
+    public void atualizarPagina();
+}
